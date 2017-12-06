be30185749cc6343e987d2290d29f02348e49c39
Damon Polioudakis <damon.polioudakis@gmail.com>
2017-12-05 17:13:32 -0800

    Telley transcriptional waves pseudotime heatmaps

--- code/Monocle_Round2_Analysis.R
+++ code/Monocle_Round2_Analysis.R
@@ -11,6 +11,7 @@ require(cowplot)
 require(ggplot2)
 require(viridis)
 require(reshape2)
+require(biomaRt)
 source("Function_Library.R")
 
 # qsub task ID for selecting Seurat cluster ID to run through Monocle
@@ -25,65 +26,65 @@ toOrder <- c("0-1-4-13", "0-1", "3-14", "5-6", "7-9", "0", "1", "2", "3", "4"
   , "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15")
 
 # Monocle object
-inMo <- list.files("../analysis/Monocle_Round2/")
-inMo <- inMo[grep("Monocle_Round2_Comp1-20_monocleO", inMo)]
+inMo <- list.files("../analysis/Monocle_Round2/FtMm250_200-3sdgd_Mt5_RegNumiLibBrain_KeepCC_PC1to40/Comp1-10/")
+inMo <- inMo[grep("Monocle_Round2_monocleO", inMo)]
 moL <- lapply(inMo, function(path) {
-  load(paste0("../analysis/Monocle_Round2/", path))
+  load(paste0("../analysis/Monocle_Round2/FtMm250_200-3sdgd_Mt5_RegNumiLibBrain_KeepCC_PC1to40/Comp1-10/", path))
   return(mo_filtered)
 })
 # List names are Seurat clusters used for Monocle round 2
-names <- gsub("Monocle_Round2_Comp1-20_monocleO_cluster", "", inMo)
+names <- gsub("Monocle_Round2_monocleO_cluster", "", inMo)
 names <- gsub(".Robj", "", names)
 names(moL) <- names
 # Reorder list of monocle objects for plotting
 moL <- moL[toOrder]
 
 # Pseudotime DE
-inPtDE <- list.files("../analysis/Monocle_Round2/")
-inPtDE <- inPtDE[grep("Monocle_Round2_Comp1-20_DEpseudotime_cluster", inPtDE)]
+inPtDE <- list.files("../analysis/Monocle_Round2/FtMm250_200-3sdgd_Mt5_RegNumiLibBrain_KeepCC_PC1to40/Comp1-10/")
+inPtDE <- inPtDE[grep("Monocle_Round2_DEpseudotime_cluster", inPtDE)]
 ptDeL <- lapply(inPtDE, function(path) {
-  load(paste0("../analysis/Monocle_Round2/", path))
+  load(paste0("../analysis/Monocle_Round2/FtMm250_200-3sdgd_Mt5_RegNumiLibBrain_KeepCC_PC1to40/Comp1-10/", path))
   return(ptDE)
 })
 # List names are Seurat clusters used for Monocle round 2
-names <- gsub("Monocle_Round2_Comp1-20_DEpseudotime_cluster", "", inPtDE)
+names <- gsub("Monocle_Round2_DEpseudotime_cluster", "", inPtDE)
 names <- gsub(".Robj", "", names)
 names(ptDeL) <- names
 # Reorder list of DE for plotting
 ptDeL <- ptDeL[toOrder]
 
 # State DE
-inStDE <- list.files("../analysis/Monocle_Round2/")
+inStDE <- list.files("../analysis/Monocle_Round2/FtMm250_200-3sdgd_Mt5_RegNumiLibBrain_KeepCC_PC1to40/Comp1-10/")
 inStDE <- inStDE[grep("DEstate_cluster", inStDE)]
 # inStDE <- inStDE[8:15]
 stDeLDF <- lapply(inStDE, function(path) {
-  load(paste0("../analysis/Monocle_Round2/", path))
+  load(paste0("../analysis/Monocle_Round2/FtMm250_200-3sdgd_Mt5_RegNumiLibBrain_KeepCC_PC1to40/Comp1-10/", path))
   # load("../analysis/Monocle_Round2/Monocle_Round2_Comp1-20_DEstate_cluster5-6.Robj")
   return(stateDE)
 })
 # List names are Seurat clusters used for Monocle round 2
-names <- gsub("Monocle_Round2_Comp1-20_DEstate_cluster", "", inStDE)
+names <- gsub("Monocle_Round2_DEstate_cluster", "", inStDE)
 names <- gsub(".Robj", "", names)
 names(stDeLDF) <- names
 # Reorder list of DE for plotting
 stDeLDF <- stDeLDF[toOrder]
 
 # Branch DE
-inBeam <- list.files("../analysis/Monocle_Round2/")
+inBeam <- list.files("../analysis/Monocle_Round2/FtMm250_200-3sdgd_Mt5_RegNumiLibBrain_KeepCC_PC1to40/Comp1-10/")
 inBeam <- inBeam[grep("DEbranch_cluster", inBeam)]
 beamLLDF <- lapply(inBeam, function(path) {
-  load(paste0("../analysis/Monocle_Round2/", path))
+  load(paste0("../analysis/Monocle_Round2/FtMm250_200-3sdgd_Mt5_RegNumiLibBrain_KeepCC_PC1to40/Comp1-10/", path))
   return(beamLDF)
 })
 # List names are Seurat clusters used for Monocle round 2
-names <- gsub("Monocle_Round2_Comp1-20_DEbranch_cluster", "", inBeam)
+names <- gsub("Monocle_Round2_DEbranch_cluster", "", inBeam)
 names <- gsub(".Robj", "", names)
 names(beamLLDF) <- names
 # Reorder list of DE for plotting
 beamLLDF <- beamLLDF[toOrder]
 
 # Seurat clustering
-load("../analysis/Seurat_Cluster_DS2-11/Seurat_Cluster_DS2-11_FtMm250_200-3sdgd_Mt5_RegNumiLibBrain_PC1to40_seuratO.Robj")
+load("../analysis/Seurat_Cluster_DS2-11/FtMm250_200-3sdgd_Mt5_RegNumiLibBrain_KeepCC_PC1to40/Seurat_Cluster_DS2-11_seuratO.Robj")
 
 # Known cell type markers from Luis
 kmDF <- read.csv("../source/MarkersforSingleCell_2017-01-05.csv", header = TRUE
@@ -91,6 +92,9 @@ kmDF <- read.csv("../source/MarkersforSingleCell_2017-01-05.csv", header = TRUE
 
 # Molyneaux markers
 mmDF <- read.csv("../source/Molyneaux_LayerMarkers_Format.csv", header = TRUE)
+
+# Telley transcriptional waves
+tyDF <- read.csv("../source/Telley_2015_ST3_TranscriptionalWaves.csv", header = TRUE)
 
 # Human TFs
 tfDF <- read.table("../source/AnimalTFDB_Homo_sapiens_TF_EnsemblID.txt")
@@ -106,17 +110,14 @@ bmDF <- read.csv("../source/BiomaRt_Compile_GeneInfo_GRCh38_Ensembl87.csv"
 
 ## Variables
 graphCodeTitle <- "Monocle_Round2_Analysis.R"
-outGraph <- "../analysis/graphs/Monocle_Round2_Analysis/Monocle_Round2_Analysis_Comp1-20_"
-outTable <- "../analysis/tables/Monocle_Round2_Analysis/Monocle_Round2_Analysis_Comp1-20_"
-outRdat <- "../analysis/Monocle_Round2_Analysis/Monocle_Round2_Analysis_Comp1-20_"
+outGraph <- "../analysis/graphs/Monocle_Round2_Analysis/Comp1-10/Monocle_Round2_"
+outTable <- "../analysis/tables/Monocle_Round2_Analysis/Comp1-10/Monocle_Round2_"
+outRdat <- "../analysis/Monocle_Round2_Analysis/Comp1-10/Monocle_Round2_"
 
 ## Output Directories
-outDir <- dirname(outGraph)
-dir.create(outDir, recursive = TRUE)
-outTableDir <- dirname(outTable)
-dir.create(outTableDir, recursive = TRUE)
-outRdatDir <- dirname(outRdat)
-dir.create(outRdatDir, recursive = TRUE)
+dir.create(dirname(outGraph), recursive = TRUE)
+dir.create(dirname(outTable), recursive = TRUE)
+dir.create(dirname(outRdat), recursive = TRUE)
 
 ## Set ggplot2 theme
 theme_set(theme_bw())
@@ -580,6 +581,82 @@ lapply(names(ptDeL), function(cluster){
     }
   )
   dev.off()
+})
+
+
+## Heatmaps of Telley transcriptional wave genes
+
+# Output Directories
+outDir <- paste0(outGraph, "Pseudotime_Heatmap_TelleyWaves")
+dir.create(outDir, recursive = TRUE)
+outSubGraph <- paste0(outDir, "/", basename(outDir), "_")
+# Plot
+lapply(names(ptDeL), function(cluster){
+  
+  diff_test_res <- ptDeL[[cluster]]
+  mo_filtered <- moL[[cluster]]
+  
+  Convert_MouseGenes_To_HumanGenes <- function(geneList) {
+    geneList <- data.frame(geneList)
+    mart = useEnsembl(biomart = "ensembl", dataset = "mmusculus_gene_ensembl")
+    mart <- useMart("ENSEMBL_MART_ENSEMBL", host = "www.ensembl.org")
+    mart <- useDataset("mmusculus_gene_ensembl", mart = mart)
+    # Look up human orthologs
+    ensemblMmHsDF <- getBM(
+      attributes = c("ensembl_gene_id", "hsapiens_homolog_ensembl_gene"
+        , "hsapiens_homolog_associated_gene_name", "hsapiens_homolog_perc_id")
+      , filters = "mgi_symbol"
+      , values = geneList
+      , mart = mart
+    )
+    # Have to add MGI symbol separately, can't call BM to add both ortholog and MGI
+    # at the same time
+    ensemblMgiSymDF <- getBM(
+      attributes = c("ensembl_gene_id", "mgi_symbol")
+      , filters = "mgi_symbol"
+      , values = geneList
+      , mart = mart
+    )
+    # Merge mouse gene symbols and human
+    genesDF <- merge(ensemblMmHsDF, ensemblMgiSymDF
+      , by.x = "ensembl_gene_id", by.y = "ensembl_gene_id")
+    return(genesDF)
+  }
+  
+  lapply(colnames(tyDF), function(wave) {
+    
+    telleyGenes <- tyDF[ ,wave]
+  
+    # Convert mouse to human
+    df1 <- Convert_MouseGenes_To_HumanGenes(geneList = telleyGenes)
+    telleyGenes <- df1$hsapiens_homolog_associated_gene_name
+    
+    # Subset to TFs, co-factors, chromatin remodelers and by qval
+    diff_test_res <- diff_test_res[diff_test_res$qval < 0.05
+      & diff_test_res$gene_short_name %in% telleyGenes, ]
+    
+    # Order by qval
+    diff_test_res <- diff_test_res[order(diff_test_res$qval), ]
+    
+    genes <- row.names(diff_test_res)
+    
+    png(paste0(outSubGraph, "Cluster", cluster, "_", wave, ".png")
+      , width = 12, height = 16, units = "in", res = 300)
+    tryCatch(plot_pseudotime_heatmap(mo_filtered[genes,]
+      , num_clusters = 3
+      , cores = 1
+      , show_rownames = T)
+      , error = function(cond) {
+        message(paste0("Error for cluster: ", cluster))
+        message("Error message:")
+        message(cond)
+        # Choose a return value in case of error
+        return(NULL)
+      }
+    )
+    dev.off()
+  
+  })
 })
 
 # Heatmap of state DE genes
