9815cac6426d22b1ea30ee223295abc4419cf13b
Damon Polioudakis <damon.polioudakis@gmail.com>
2019-01-15 18:29:47 -0800

    Add TF and disease gene list heatmaps

--- code/Seurat_ClusterRound2_DE_Plots.R
+++ code/Seurat_ClusterRound2_DE_Plots.R
@@ -50,12 +50,12 @@ cluster_annot_tb <- tribble(
 
 ## inputs
 # sub-clustering seurat dir
-in_subclust_so_dir <- "../analysis/analyzed_data/Seurat_ClusterRound2/ClusterRound2/20181222/"
+in_subclust_so_dir <- "../analysis/analyzed_data/Seurat_ClusterRound2/ClusterRound2/20190103/"
 # clustering to use
-cluster_col <- "clusters_pc1to10_res_0.5"
+cluster_col <- "clusters_pc1to5_res_0.7"
 # sub-clustering DE enrichment tables
 # (DE sub-cluster vs other sub-clusters in the cluster)
-in_subclust_de_dir <- "../analysis/tables/Seurat_ClusterRound2/DE/20190102/"
+in_subclust_de_dir <- "../analysis/tables/Seurat_ClusterRound2/DE/20181228/"
 # sub-clustering DE enrichment vs full dataset tables
 # (DE sub-cluster vs all other cells in dataset)
 subclust_de_vs_40k_tb <- read_csv("../analysis/tables/Seurat_ClusterRound2/DE_vs_40k/20181210/Seurat_ClusterRound2_DE_vs_40k_ClusterX_Vs_All_Clusters.csv")
@@ -65,22 +65,47 @@ load("../analysis/analyzed_data/Seurat_Cluster_DS2-11/FtMm250_200-3sdgd_Mt5_RegN
 # centSO <- ssCentSO
 # noCentExM <- ssNoCentExM
 # cluster DE enrichment table
-# Cluster DE table
+# cluster DE table
 cluster_de_tb <- read_tsv(
   "../analysis/tables/Seurat_ClusterDE/DS2-11/FtMm250_200-3sdgd_Mt5_RegNumiLibBrain_KeepCC_PC1to40/res054/Seurat_ClusterDE_ClusterX_Vs_All_Clusters.txt")
 
+# human transcriptional regulators
+tr_tb <- bind_rows(
+  # TFs
+  read_table("../source/AnimalTFDB_Homo_sapiens_TF_EnsemblID.txt"
+    , col_names = FALSE)
+  # human chromatin remodeling factors
+  , read_table(
+  "../source/AnimalTFDB_Homo_sapiens_chr_remodeling_factor_EnsemblID.txt"
+    , col_names = FALSE)
+  # human co-factors
+  , read_table("../source/AnimalTFDB_Homo_sapiens_cofactor_EnsemblID.txt"
+    , col_names = FALSE)
+)
+
+# Disease risk gene datasets
+asd_tada_tb <- load_asd_sanders_genes()
+asd_ihart_tb <- load_asd_ihart_genes()
+epilepsy_tb <- load_epilepsy_high_conf_genes()
+id_genes_tb <- load_id_nejm_lancet_genes()
+
 ## outputs
 out_graph <- paste0(
-  "../analysis/graphs/Seurat_ClusterRound2/DE/", date, "_pc1to10_res_0.5/Seurat_ClusterRound2_")
+  "../analysis/graphs/Seurat_ClusterRound2/DE/", date, "/Seurat_ClusterRound2_")
 ################################################################################
 
 ### main function
 
 main_function <- function(){
   # run_plot_de_vs_40k_expression_heatmaps(cluster_col = cluster_col)
-  run_plot_de_vs_subcluster_expression_heatmaps(cluster_col = cluster_col)
-  run_plot_subcluster_expression_heatmaps(cluster_col = cluster_col)
-  run_plot_enriched_gene_overlap_odds_ratio_heatmaps(cluster_col = cluster_col)
+  # run_plot_de_vs_subcluster_expression_heatmaps(cluster_col = cluster_col)
+  # run_plot_subcluster_expression_heatmaps(cluster_col = cluster_col)
+  # run_plot_enriched_gene_overlap_odds_ratio_heatmaps(cluster_col = cluster_col)
+  # run_plot_de_tr_vs_subcluster_expression_heatmaps(cluster_col = cluster_col)
+  plot_de_asd_vs_subcluster_expression_heatmaps(cluster_col = cluster_col)
+  plot_de_epilepsy_vs_subcluster_expression_heatmaps(cluster_col = cluster_col)
+  plot_de_id_vs_subcluster_expression_heatmaps(cluster_col = cluster_col)
+  plot_de_genes_oRG_subcluster_intersected_with_shin_nsc_genes()
 }
 ################################################################################
 
@@ -172,7 +197,7 @@ plot_subcluster_expression_heatmaps <- function(
   ))
   ggsave(
     paste0(out_graph, "cluster", cluster_id, "_subclust_expression_heatmap.png")
-      , height = graph_height, width = 13)
+      , height = graph_height, width = 9)
 }
 
 run_plot_subcluster_expression_heatmaps <- function(cluster_col){
@@ -782,9 +807,406 @@ run_plot_enriched_gene_overlap_odds_ratio_heatmaps <- function(){
 }
 ################################################################################
 
-subclust_de_tb %>%
-  filter(Cluster == 6) %>%
-  filter(Gene %in% c("DLX2", "DLX5", "DLX6"))
+### TRs differentially expressed by sub-cluster
+
+plot_de_tr_vs_subcluster_expression_heatmaps <- function(
+  cluster_id, seurat_obj, noCent40kExM, cluster_col){
+
+  print("plot_de_vs_subcluster_expression_heatmaps")
+  print(paste0("cluster: ", cluster_id))
+
+  # load seurat clustering round 2 object
+  in_subclust_so <- paste0(
+    in_subclust_so_dir
+    , "Seurat_ClusterRound2_Cluster", cluster_id, "_seuratO.Robj")
+  # so, noCentExM, rd1CentExM
+  load(in_subclust_so)
+
+  # set clustering to use
+  cluster_ids <- factor(so@meta.data[[cluster_col]])
+  names(cluster_ids) <- rownames(so@meta.data)
+  so@ident <- cluster_ids
+
+  # Gather cell IDs and cluster IDs (including SP subcluster) for heatmap
+  cellid_clusterid <- seurat_obj@ident %>% enframe(
+    name = "cell_id", value = "cluster_ids") %>%
+    left_join(
+      (so@ident %>% enframe(name = "cell_id", value = "sub_cluster_id"))
+      , by = "cell_id") %>%
+    mutate_if(is.factor, as.character) %>%
+    mutate(cluster_ids = if_else(cluster_ids == cluster_id, paste0(.$cluster_ids, "_", .$sub_cluster_id), cluster_ids))  %>%
+    left_join(cluster_annot_tb
+      , by = c("cluster_ids" = "cluster_number")) %>%
+      select(cell_id, cluster_ids) %>% deframe
+
+  # standardize column formatting
+  subclust_de_tb <- subclust_de_tb %>%
+    mutate(Cluster_Subcluster = paste0(Cluster, "_", Subcluster)) %>%
+    mutate(Cluster = as.character(Cluster))
+
+  gene_group_df <-
+    subclust_de_tb %>%
+    # subset to enrichment filters
+    filter(Cluster == cluster_id & FDR < 0.05 & Log2_Fold_Change > 0.2) %>%
+    # subset to TRs
+    filter(Ensembl %in% pull(tr_tb)) %>%
+    group_by(Subcluster) %>%
+    arrange(Subcluster, Log2_Fold_Change) %>%
+    select(Gene = Gene, Group = Subcluster) %>%
+    as.data.frame()
+
+  expr_m <- as.matrix(seurat_obj@scale.data)
+
+  cluster_annot <- cluster_annot_tb %>%
+    filter(cluster_number == cluster_id) %>%
+    pull(cluster_annot)
+
+  cluster_order <- cluster_annot_tb %>%
+    left_join(.
+      , subclust_de_tb %>%
+          filter(Cluster == cluster_id) %>%
+          select(Cluster, Cluster_Subcluster, Subcluster) %>%
+          distinct %>%
+          arrange(Subcluster) %>%
+          mutate(Cluster = as.character(Cluster))
+      , by = c("cluster_number" = "Cluster")) %>%
+    mutate(cluster_number = if_else(
+      cluster_number == cluster_id, Cluster_Subcluster, cluster_number)) %>%
+    pull(cluster_number)
+
+  graph_height = 2 + nrow(gene_group_df)*0.25
+
+  Plot_Marker_Genes_Heatmap_SetColWidths(
+    geneGroupDF = gene_group_df
+    , exprM = expr_m
+    , cellID_clusterID = cellid_clusterid
+    , clusters = cellid_clusterid %>% unique
+    , clusterOrder = cluster_order
+    , lowerLimit = -1.5
+    , upperLimit = 1.5
+  ) + ggtitle(paste0(
+    script_name
+    , "\n\nExpression of transcriptional regulators enriched for sub-clusters of cluster: "
+    , cluster_annot, " (", cluster_id, ")"
+    , "\nEnrichment determined by DE of cells in sub-cluster versus other in cluster"
+    , "\nx-axis: Genes"
+    , "\ny-axis: Cells ordered by cluster"
+    , "\nNormalized expression, mean centered variance scaled by gene"
+  ))
+  ggsave(
+    paste0(out_graph, "cluster", cluster_id
+    , "_de_tr_vs_subclust_expression_heatmap_zscore.png")
+    , height = graph_height, width = 13, limitsize = FALSE)
+
+  Plot_Marker_Genes_Heatmap_SetColWidths(
+    geneGroupDF = gene_group_df
+    , exprM = noCent40kExM
+    , cellID_clusterID = cellid_clusterid
+    , clusters = cellid_clusterid %>% unique
+    , clusterOrder = cluster_order
+    , lowerLimit = 0
+    , upperLimit = 4
+    , color_viridis = TRUE
+  ) + ggtitle(paste0(
+    script_name
+    , "\n\nExpression of top 30 enriched genes for sub-clusters of cluster: "
+    , cluster_annot, " (", cluster_id, ")"
+    , "\nEnrichment determined by DE of cells in sub-cluster versus other in cluster"
+    , "\nx-axis: Genes"
+    , "\ny-axis: Cells ordered by cluster"
+    , "\nNormalized expression"
+  ))
+  ggsave(
+    paste0(out_graph, "cluster", cluster_id,
+     "_de_tr_vs_subclust_expression_heatmap.png")
+      , height = graph_height, width = 13, limitsize = FALSE)
+}
+
+run_plot_de_tr_vs_subcluster_expression_heatmaps <- function(cluster_col){
+
+  print("run_plot_de_tr_vs_subcluster_expression_heatmaps")
+
+  cluster_annot_tb$cluster_number %>%
+    map(
+      ~tryCatch({
+          plot_de_tr_vs_subcluster_expression_heatmaps(
+            seurat_obj = centSO
+            , noCent40kExM = noCentExM
+            , cluster_id = .
+            , cluster_col = cluster_col
+          )
+        }
+          , error = function(cond){
+            message(paste0("Error for cluster: ", .))
+            message(paste0(cond, "\n"))
+            return(NA)
+        }
+    ))
+}
+################################################################################
+
+### Disease genes differentially expressed by sub-cluster
+
+plot_de_genes_of_interest_vs_subcluster_expression_heatmaps <- function(
+  cluster_id, seurat_obj, noCent40kExM, cluster_col, genes
+  , gene_list_plot_title, out_graph_pfx){
+
+  print("plot_de_genes_of_interest_vs_subcluster_expression_heatmaps")
+  print(paste0("cluster: ", cluster_id))
+
+  # load seurat clustering round 2 object
+  in_subclust_so <- paste0(
+    in_subclust_so_dir
+    , "Seurat_ClusterRound2_Cluster", cluster_id, "_seuratO.Robj")
+  # so, noCentExM, rd1CentExM
+  load(in_subclust_so)
+
+  # set clustering to use
+  cluster_ids <- factor(so@meta.data[[cluster_col]])
+  names(cluster_ids) <- rownames(so@meta.data)
+  so@ident <- cluster_ids
+
+  # Gather cell IDs and cluster IDs for heatmap
+  cellid_clusterid <- seurat_obj@ident %>% enframe(
+    name = "cell_id", value = "cluster_ids") %>%
+    left_join(
+      (so@ident %>% enframe(name = "cell_id", value = "sub_cluster_id"))
+      , by = "cell_id") %>%
+    mutate_if(is.factor, as.character) %>%
+    mutate(cluster_ids = if_else(cluster_ids == cluster_id, paste0(.$cluster_ids, "_", .$sub_cluster_id), cluster_ids))  %>%
+    left_join(cluster_annot_tb
+      , by = c("cluster_ids" = "cluster_number")) %>%
+      select(cell_id, cluster_ids) %>% deframe
+
+  # standardize column formatting
+  subclust_de_tb <- subclust_de_tb %>%
+    mutate(Cluster_Subcluster = paste0(Cluster, "_", Subcluster)) %>%
+    mutate(Cluster = as.character(Cluster))
+
+  gene_group_df <-
+    subclust_de_tb %>%
+    # subset to enrichment filters
+    filter(Cluster == cluster_id & FDR < 0.05 & Log2_Fold_Change > 0.2) %>%
+    # subset to TRs
+    filter(Gene %in% genes) %>%
+    group_by(Subcluster) %>%
+    arrange(Subcluster, Log2_Fold_Change) %>%
+    select(Gene = Gene, Group = Subcluster) %>%
+    as.data.frame()
+
+  expr_m <- as.matrix(seurat_obj@scale.data)
+
+  cluster_annot <- cluster_annot_tb %>%
+    filter(cluster_number == cluster_id) %>%
+    pull(cluster_annot)
+
+  cluster_order <- cluster_annot_tb %>%
+    left_join(.
+      , subclust_de_tb %>%
+          filter(Cluster == cluster_id) %>%
+          select(Cluster, Cluster_Subcluster, Subcluster) %>%
+          distinct %>%
+          arrange(Subcluster) %>%
+          mutate(Cluster = as.character(Cluster))
+      , by = c("cluster_number" = "Cluster")) %>%
+    mutate(cluster_number = if_else(
+      cluster_number == cluster_id, Cluster_Subcluster, cluster_number)) %>%
+    pull(cluster_number)
+
+  graph_height = 2 + nrow(gene_group_df)*0.25
+
+  Plot_Marker_Genes_Heatmap_SetColWidths(
+    geneGroupDF = gene_group_df
+    , exprM = expr_m
+    , cellID_clusterID = cellid_clusterid
+    , clusters = cellid_clusterid %>% unique
+    , clusterOrder = cluster_order
+    , lowerLimit = -1.5
+    , upperLimit = 1.5
+  ) + ggtitle(paste0(
+    script_name
+    , "\n\nExpression of genes enriched for sub-clusters of cluster: "
+    , cluster_annot, " (", cluster_id, ")"
+    , gene_list_plot_title
+    , "\nEnrichment determined by DE of cells in sub-cluster versus other in cluster"
+    , "\nx-axis: Genes"
+    , "\ny-axis: Cells ordered by cluster"
+    , "\nNormalized expression, mean centered variance scaled by gene"
+  ))
+  ggsave(
+    paste0(out_graph_pfx, "_zscore.png")
+    , height = graph_height, width = 13, limitsize = FALSE)
+
+  Plot_Marker_Genes_Heatmap_SetColWidths(
+    geneGroupDF = gene_group_df
+    , exprM = noCent40kExM
+    , cellID_clusterID = cellid_clusterid
+    , clusters = cellid_clusterid %>% unique
+    , clusterOrder = cluster_order
+    , lowerLimit = 0
+    , upperLimit = 4
+    , color_viridis = TRUE
+  ) + ggtitle(paste0(
+    script_name
+    , "\n\nExpression of genes enriched for sub-clusters of cluster: "
+    , cluster_annot, " (", cluster_id, ")"
+    , gene_list_plot_title
+    , "\nEnrichment determined by DE of cells in sub-cluster versus other in cluster"
+    , "\nx-axis: Genes"
+    , "\ny-axis: Cells ordered by cluster"
+    , "\nNormalized expression"
+  ))
+  ggsave(
+    paste0(out_graph_pfx, ".png")
+      , height = graph_height, width = 13, limitsize = FALSE)
+}
+
+plot_de_asd_vs_subcluster_expression_heatmaps <- function(cluster_col){
+
+  print("plot_de_asd_vs_subcluster_expression_heatmaps")
+
+  cluster_annot_tb$cluster_number %>%
+    map(
+      ~tryCatch({
+          plot_de_genes_of_interest_vs_subcluster_expression_heatmaps(
+            seurat_obj = centSO
+            , noCent40kExM = noCentExM
+            , cluster_id = .
+            , cluster_col = cluster_col
+            , genes = pull(asd_tada_tb)
+            , gene_list_plot_title = "ASD risk genes (Sander et al.)"
+            , out_graph_pfx = paste0(out_graph, "cluster", .
+              , "_de_asd_vs_subclust_expression_heatmap")
+          )
+        }
+          , error = function(cond){
+            message(paste0("Error for cluster: ", .))
+            message(paste0(cond, "\n"))
+            return(NA)
+        }
+    ))
+}
+
+plot_de_id_vs_subcluster_expression_heatmaps <- function(cluster_col){
+
+  print("plot_de_id_vs_subcluster_expression_heatmaps")
+
+  cluster_annot_tb$cluster_number %>%
+    map(
+      ~tryCatch({
+          plot_de_genes_of_interest_vs_subcluster_expression_heatmaps(
+            seurat_obj = centSO
+            , noCent40kExM = noCentExM
+            , cluster_id = .
+            , cluster_col = cluster_col
+            , genes = pull(id_genes_tb)
+            , gene_list_plot_title = "ID risk genes"
+            , out_graph_pfx = paste0(out_graph, "cluster", .
+              , "_de_id_vs_subclust_expression_heatmap")
+          )
+        }
+          , error = function(cond){
+            message(paste0("Error for cluster: ", .))
+            message(paste0(cond, "\n"))
+            return(NA)
+        }
+    ))
+}
+
+plot_de_epilepsy_vs_subcluster_expression_heatmaps <- function(cluster_col){
+
+  print("plot_de_epilepsy_vs_subcluster_expression_heatmaps")
+
+  cluster_annot_tb$cluster_number %>%
+    map(
+      ~tryCatch({
+          plot_de_genes_of_interest_vs_subcluster_expression_heatmaps(
+            seurat_obj = centSO
+            , noCent40kExM = noCentExM
+            , cluster_id = .
+            , cluster_col = cluster_col
+            , genes = pull(epilepsy_tb)
+            , gene_list_plot_title = "Epilepsy risk genes (Ruzzo)"
+            , out_graph_pfx = paste0(out_graph, "cluster", .
+              , "_de_epilepsy_vs_subclust_expression_heatmap")
+          )
+        }
+          , error = function(cond){
+            message(paste0("Error for cluster: ", .))
+            message(paste0(cond, "\n"))
+            return(NA)
+        }
+    ))
+}
+################################################################################
+
+### plot DE genes between oRG sub-cluster 0 and 1 intersected with Shin genes
+
+plot_de_genes_oRG_subcluster_intersected_with_shin_nsc_genes <- function(){
+
+  print("plot_de_genes_oRG_subcluster_intersected_with_shin_nsc_genes")
+
+  cluster_id <- 7
+
+  # load seurat clustering round 2 object
+  in_subclust_so <- paste0(
+    in_subclust_so_dir
+    , "Seurat_ClusterRound2_Cluster", cluster_id, "_seuratO.Robj")
+  # so, noCentExM, rd1CentExM
+  load(in_subclust_so)
+
+  # set clustering to use
+  cluster_ids <- factor(so@meta.data[[cluster_col]])
+  names(cluster_ids) <- rownames(so@meta.data)
+  so@ident <- cluster_ids
+
+  # genes associated with adult NSCs trajectory analysis
+  shin_tfs <- read_csv(
+    "../tmp/shin2015_table_s5.csv", skip = 2, col_names = TRUE) %>%
+    clean_variable_names %>% mutate_all(toupper)
+  shin_genes <- read_csv(
+    "../tmp/shin2015_table_s4.csv", skip = 2, col_names = TRUE) %>%
+    clean_variable_names %>% mutate_all(toupper)
+
+  # DE genes between oRG sub-cluster 0 and 1
+  fold_changes <- (noCentExM[ ,so@ident == 0] %>% rowMeans) - (noCentExM[ ,so@ident == 1] %>% rowMeans)
+
+  # plot DE genes between oRG sub-cluster 0 and 1 intersected with Shin genes
+  fold_changes[abs(fold_changes) > 0.25] %>%
+    as.list %>%
+    as_tibble() %>%
+    gather() %>%
+    arrange(value) %>%
+    mutate(shin_genes = if_else(
+        key %in% shin_genes$upregulated[1:150], "shin_up"
+      , if_else(
+        key %in% shin_genes$downregulated[1:150], "shin_down", NULL
+      ))) %>%
+    mutate(key = factor(key, levels = key)) %>%
+    ggplot(aes(x = key, y = value, fill = shin_genes)) +
+      geom_bar(stat = "identity") +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1))
+    ggsave(paste0(out_graph, "example_pairwise_de_shin_genes_barplot.png")
+      , width = 30)
+
+  # plot DE genes between oRG sub-cluster 0 and 1 intersected with Shin TFs
+  fold_changes[abs(fold_changes) > 0.25] %>%
+    as.list %>%
+    as_tibble() %>%
+    gather() %>%
+    arrange(value) %>%
+    mutate(shin_tfs = if_else(
+        key %in% shin_tfs$up_tfs[1:150], "shin_up"
+      , if_else(
+        key %in% shin_tfs$down_tfs[1:150], "shin_down", NULL
+      ))) %>%
+    mutate(key = factor(key, levels = key)) %>%
+    ggplot(aes(x = key, y = value, fill = shin_tfs)) +
+      geom_bar(stat = "identity") +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1))
+    ggsave(paste0(out_graph, "example_pairwise_de_shin_tfs_barplot.png")
+      , width = 30)
+}
 ################################################################################
 
 ### Run
